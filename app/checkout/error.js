8cca94f6df460faf5997afd0030ad8416575fee9
Ismael Rumzan <ismael.rumzan@gmail.com>
2022-09-07 12:54:20 +0300

    error boundary

---
+++ app/checkout/error.js
@@ -0,0 +1,7 @@
+export default function Error() {
+  return (
+    <div>
+      <h3>Error found</h3>
+    </div>
+  );
+}
--- app/checkout/page.client.js
+++ app/checkout/page.client.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
+useState;
 export default function Cart() {
+  const [count, setCount] = useState(0);
+
+  function changeCount() {
+    setCount(count + 1);
+    if (count === 5) {
+      throw new Error("I crashed");
+    }
+  }
   return (
     <>
       <h1>My Cart</h1>
+      <h2>No. of items: {count}</h2>
+      <button onClick={() => changeCount()}>Add Item</button>
     </>
   );
 }
